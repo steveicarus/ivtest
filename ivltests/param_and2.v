3b87846955543d4a8005c4aa631bae2b9eeae95c
stevewilliams <stevewilliams>
2001-01-04 04:27:48 +0000

    & is a LOGICAL and.

--- ivltests/param_and2.v
+++ ivltests/param_and2.v
@@ -22,12 +22,12 @@
  */
 module test;
 
-parameter A0 = 2'b10 && 2'b10 ;
+parameter A0 = 2'b10 && 2'b01 ;
 
 initial 
   begin
-   if(A0 !== 2'b10)
-      $display("FAILED - A0 expression OR doesn't work.");
+   if(A0 !== 1'b1)
+      $display("FAILED - A0 expression && doesn't work.");
    else
       $display("PASSED");
   end
