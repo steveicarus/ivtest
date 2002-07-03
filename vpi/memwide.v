1f2d421779c0f583f86b788c9364fa05ae4dc4b9
stevewilliams <stevewilliams>
2002-07-03 23:46:20 +0000

    More complete memwide test.

--- vpi/memwide.v
+++ vpi/memwide.v
@@ -24,12 +24,15 @@ module test;
   reg		event_trigger;
 
   initial begin
-    #0  big_reg=33'h1_2345_6789;
-        my_mem[0]=33'h1_2345_6789;
     #10 $display("!!!VERILOG: big_reg=%h",big_reg);
-        $display("            my_mem[0]=%h",my_mem[0]);
+        $display("            my_mem[1]=%h",my_mem[1]);
         event_trigger=1;
-    #20 $finish;
+    #10 big_reg=33'h1_2345_6789;
+        my_mem[1]=33'h1_5432_9876;
+    #10 $display("!!!VERILOG: big_reg=%h",big_reg);
+        $display("            my_mem[1]=%h",my_mem[1]);
+        event_trigger=!event_trigger;
+    #10 $finish;
   end
 
 endmodule
