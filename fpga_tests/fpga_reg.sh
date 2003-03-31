5145f46b5e5369e47cafe7896e400bf81852ebee
stevewilliams <stevewilliams>
2003-03-31 01:16:43 +0000

    Proper copyright statement.

--- fpga_tests/fpga_reg.sh
+++ fpga_tests/fpga_reg.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+#
+#    This source code is free software; you can redistribute it
+#    and/or modify it in source code form under the terms of the GNU
+#    Library General Public License as published by the Free Software
+#    Foundation; either version 2 of the License, or (at your option)
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Library General Public License for more details.
+#
+#    You should have received a copy of the GNU Library General Public
+#    License along with this program; if not, write to the Free
+#    Software Foundation, Inc.,
+#    59 Temple Place - Suite 330
+#    Boston, MA 02111-1307, USA
+#
+#ident "$Id: fpga_reg.sh,v 1.3 2003/03/31 01:16:43 stevewilliams Exp $"
+
 # This script runs the synthesis tests listed in the fpga_reg.list
 # list file. The script uses Icarus Verilog from the path, and also
 # gets ngdbuild and ngd2ver from the path. The XILINX variable needs
