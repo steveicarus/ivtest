e53cf38c82bf03dbffbea91473a7e9677f34b1b2
stevewilliams <stevewilliams>
2004-06-18 16:39:25 +0000

    Make pr993 more thorough.

--- ivltests/pr993.v
+++ ivltests/pr993.v
@@ -9,9 +9,20 @@ module main;
    integer length;
    wire [31:0] length_bits  = ((length * 8 )/11)+(((length * 8 )%11) != 0);
 
+   reg [31:0] length_bits2;
+
    initial begin
-      for (length = 1 ;  length < 25 ;  length = length + 1)
-	 #1 $display("length=%3d, length_bits=%3d", length, length_bits);
+      for (length = 1 ;  length < 56 ;  length = length + 1) begin
+	 length_bits2 = ((length * 8 )/11)+(((length * 8 )%11) != 0);
+
+	 #1 $display("length=%3d, length_bits=%3d (%3d)",
+		     length, length_bits, length_bits2);
+
+	 if (length_bits != length_bits2) begin
+	    $display("FAILED - Expressions have different results.");
+	    $finish;
+	 end
+      end // for (length = 1 ;  length < 56 ;  length = length + 1)
 
       $finish;
    end
