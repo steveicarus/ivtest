4d0cb88400a7c52f50b1725f38d333e0cc501626
stevewilliams <stevewilliams>
2004-09-04 04:24:42 +0000

    Add pr1026

---
+++ ivltests/pr1026.v
@@ -0,0 +1,22 @@
+
+/*
+ * This is based on PR#1026.
+ */
+module main;
+
+   reg [4:0] index;
+   reg [31:0] foo;
+
+   initial begin
+      for (index = 0 ;  index < 31 ;  index = index + 1) begin
+	 #1 $display("index=%d, foo=%b", index, foo);
+      end
+      $finish;
+   end
+
+   always @(*)
+     begin
+	foo = 32'b0;
+	foo[index]=1'b1;
+     end
+endmodule
