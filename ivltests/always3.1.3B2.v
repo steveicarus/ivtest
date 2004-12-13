0aa5e72097fef60de0829ecdd2efaf82edfc9c8a
stevewilliams <stevewilliams>
2004-12-13 18:34:25 +0000

    formatting fixes.

--- ivltests/always3.1.3B2.v
+++ ivltests/always3.1.3B2.v
@@ -27,14 +27,13 @@ module main ;
 
 reg [3:0] value1 ;
 
-initial 
+initial begin
     #3 if(value1 != 4'h1)
   	$display("FAILED - 3.1.3B always assign reg_lvalue = boolean_expr"); 
     else
-	begin
-           $display("PASSED");
-     	   $finish;                                                            
-        end
+        $display("PASSED");
+    $finish;                                                            
+end
 
 always #2 assign value1 = 1'b1 && 1'b1 ; 
 
