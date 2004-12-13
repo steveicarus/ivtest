343ff6112297372928f224ff5226a11553e66cd2
stevewilliams <stevewilliams>
2004-12-13 18:34:49 +0000

    Fix PASSED test

--- ivltests/always3.1.3D2.v
+++ ivltests/always3.1.3D2.v
@@ -28,13 +28,11 @@ reg [3:0] value1 ;
 initial 
   begin
     #15;
-    if(value1 != 4'h5)
-      $display("FAILED - 3.1.3D always force reg_lvalue = constant;\n"); 
+    if(value1 !== 4'h5)
+      $display("FAILED - 3.1.3D always force reg_lvalue = constant;"); 
     else
-	begin
-           $display("PASSED\n");
-     	   $finish;                                                            
-        end
+      $display("PASSED");
+    $finish;
   end
 
 always #10 force value1 = 4'h5;
