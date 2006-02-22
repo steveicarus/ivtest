0e790c9a06f1085256b9d8ad099ad5544e8bd471
stevewilliams <stevewilliams>
2006-02-22 05:46:14 +0000

    Fix a time-0 race with always statement.

--- vpi/putvalue.v
+++ vpi/putvalue.v
@@ -7,7 +7,7 @@ module test;
     reg r;
     initial begin
 	$monitor("<monitor> r = ", r);
-	r = 1'b0;
+	#0.1 r = 1'b0;
 	#100000 $finish;
     end
     always @(r) $display("<display> r = %b @ %0t", r, $time);
