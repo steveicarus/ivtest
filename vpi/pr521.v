6303100c9135b864fd46b435f0cf6a68e0119734
stevewilliams <stevewilliams>
2006-02-23 00:51:44 +0000

    Fix time-0 race condition.

--- vpi/pr521.v
+++ vpi/pr521.v
@@ -7,6 +7,7 @@ integer rc;
 
 initial
  begin
+   #1 /* Allow the continuous assignments above to settle. */ ;
    $display("Passing parameter to PLI routine: 0x%x",a[15:8]);
    rc = $pli_test(a[15:8]);
    $display("Passing parameter to PLI routine: 0x%x",b);
