5284f690404d6884679bee57f270c46836e6a32f
stevewilliams <stevewilliams>
2007-01-19 04:50:35 +0000

    Add tests pr1632861, pr1637208

---
+++ ivltests/pr1632861.v
@@ -0,0 +1,21 @@
+module test;
+  reg[9:0] tst;
+
+  initial begin
+    #1 tst = 0;
+    // This should set the register to 10'b00000xxxxx!
+    #1 tst = 5'hxx;
+    #1 tst = 10'h3ff;
+    #1 tst = 10'b00000xxxxx;
+    #1 tst = 0;
+    #1 tst = 8'hxx;
+    #1 tst = 0;
+    $finish;
+  end
+
+  always @(tst) begin
+    $display("At %0t value is %b", $time, tst);
+  end
+endmodule
+
+ 	  	 
