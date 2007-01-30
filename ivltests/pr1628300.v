c59b4dca6105d571d629da4d4379d82d37065aa3
stevewilliams <stevewilliams>
2007-01-30 05:38:42 +0000

    Add pr1628300 and pr1639968.

---
+++ ivltests/pr1628300.v
@@ -0,0 +1,17 @@
+module bug;
+
+function real sin;
+	input x;
+	real x;
+	sin = 1.570794*x;
+endfunction
+
+real ax, ay;
+initial begin
+	ax = 2.0;
+	ay = sin(ax);
+	$display("sin(", ax, ") is not really ", ay);
+end
+endmodule
+
+ 	  	 
