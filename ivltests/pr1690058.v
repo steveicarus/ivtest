4aab09cfb886a34af7a698dca2e58d256e54bb41
stevewilliams <stevewilliams>
2007-04-04 02:34:13 +0000

    Add tests pr1690058, pr1693921.

---
+++ ivltests/pr1690058.v
@@ -0,0 +1,16 @@
+
+module test;
+
+   parameter bar = 8;
+   localparam foo = 2**bar;
+
+   initial begin
+      if (foo != 'h100) begin
+	 $display("FAILED -- foo = %h", foo);
+	 $finish;
+      end
+
+      $display("PASSED");
+   end
+
+endmodule
