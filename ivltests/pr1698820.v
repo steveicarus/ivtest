4c71728dbccca3b0ce18e1559f97435baf5b62e0
stevewilliams <stevewilliams>
2007-04-15 02:22:35 +0000

    Add pr1698820.

---
+++ ivltests/pr1698820.v
@@ -0,0 +1,20 @@
+module top;
+  reg [4:0] var;
+  integer fp;
+
+  initial begin
+    var = 10;
+    fp = 1;
+    $fdisplay(fp, "The variable is ",var);
+    $fdisplayb(fp, "The variable is ",var);
+    $fdisplayo(fp, "The variable is ",var);
+    $fdisplayh(fp, "The variable is ",var);
+    $fwrite(fp, "The variable is ",var, "\n");
+    $fwriteb(fp, "The variable is ",var, "\n");
+    $fwriteo(fp, "The variable is ",var, "\n");
+    $fwriteh(fp, "The variable is ",var, "\n");
+    $fclose(fp);
+  end
+endmodule
+
+ 	  	 
