5a66f332a1af1596f20c22e98818bf4f1996ba53
stevewilliams <stevewilliams>
2007-10-10 01:08:27 +0000

    Add urand and urand_r tests.

---
+++ ivltests/urand_r.v
@@ -0,0 +1,13 @@
+module top;
+  reg [31:0] var;
+
+  initial begin
+    $monitor(var);
+    var = 0;
+    repeat (60) begin
+       #1 var = $urandom_range(16,0);
+//       #1 var = $urandom_range(4294967295,0);
+//       #1 var = $urandom_range(-1,0);
+    end
+  end
+endmodule
