8ea3095be1a184e40faefc9ee32fcc2b09618a7a
stevewilliams <stevewilliams>
2007-11-16 03:29:04 +0000

    Update and improve the extend.v test (Evan Lavelle)

--- ivltests/extend.v
+++ ivltests/extend.v
@@ -19,43 +19,100 @@
 
 /*
  * This test tests simple zero-extend of small r-values into large
- * l-values. Notice how x values are an exception.
+ * l-values. 
+ * 
+ * Correction and extensions by EML; 2007-11-15
  */
 module main;
 
    reg [3:0] y;
-   reg 	     x;
+   reg signed xs;
+   reg 	      x;
+   reg 	      fail;
 
    initial begin
+      fail = 0;
       x = 1'b0;
       y = x;
       if (y !== 4'b0000) begin
-	 $display("FAILED -- x=%b, y=%b", x, y);
-	 $finish;
+	 $display("FAILED 1 -- x=%b, y=%b", x, y);
+	 fail = 1;
       end
 
       x = 1'b1;
       y = x;
       if (y !== 4'b0001) begin
-	 $display("FAILED -- x=%b, y=%b", x, y);
-	 $finish;
+	 $display("FAILED 2 -- x=%b, y=%b", x, y);
+	 fail = 1;
       end
 
+      // x is a 1-bit unsigned reg; it zero-extends when assigned to y
       x = 1'bx;
       y = x;
-      if (y !== 4'bxxxx) begin
-	 $display("FAILED -- x=%b, y=%b", x, y);
-	 $finish;
+      if (y !== 4'b000x) begin
+	 $display("FAILED 3 -- x=%b, y=%b", x, y);
+	 fail = 1;
       end
 
+      // x is a 1-bit unsigned reg; it zero-extends when assigned to y
       x = 1'bz;
       y = x;
       if (y !== 4'b000z) begin
-	 $display("FAILED -- x=%b, y=%b", x, y);
-	 $finish;
+	 $display("FAILED 4 -- x=%b, y=%b", x, y);
+	 fail = 1;
       end
 
-      $display("PASSED");
+      // xs is a 1-bit signed reg; it top-bit-extends when assigned to y
+      xs = 1'bx;
+      y = xs;
+      if (y !== 4'bxxxx) begin
+	 $display("FAILED 5 -- xs=%b, y=%b", xs, y);
+	 fail = 1;
+      end
+
+      // xs is a 1-bit signed reg; it top-bit-extends when assigned to y
+      xs = 1'bz;
+      y = xs;
+      if (y !== 4'bzzzz) begin
+	 $display("FAILED 6 -- xs=%b, y=%b", xs, y);
+	 fail = 1;
+      end
+
+      // 'bx is an unsized unsigned constant; it X-extends to the size of 
+      // the expression it is in
+      y = 'bx;
+      if (y !== 4'bxxxx) begin
+	 $display("FAILED 7 -- y=%b", y);
+	 fail = 1;
+      end
+
+      // 'bz is an unsized unsigned constant; it Z-extends to the size of
+      // the expression it is in
+      y = 'bz;
+      if (y !== 4'bzzzz) begin
+	 $display("FAILED 8 -- y=%b", y);
+	 fail = 1;
+      end
+
+      // this is the only case in which a constant pads to the left with 
+      // X's. 4'bx is 4-bit unsigned, but it is specified with fewer than 4 
+      // bits
+      y = 4'bx;
+      if (y !== 4'bxxxx) begin
+	 $display("FAILED 9 -- y=%b", y);
+	 fail = 1;
+      end
+
+      // this is the only case in which a constant pads to the left with 
+      // Z's. 4'bz is 4-bit unsigned, but it is specified with fewer than 4 
+      // bits
+      y = 4'bz;
+      if (y !== 4'bzzzz) begin
+	 $display("FAILED 10 -- y=%b", y);
+	 fail = 1;
+      end
+
+      if (!fail) $display("PASSED");
    end // initial begin
 
 endmodule // main
