60de11e10f9c3c4030e3d7eaa241c01c9d55430c
stevewilliams <stevewilliams>
2008-04-16 00:40:05 +0000

    Add multi_bit_strength test.

---
+++ ivltests/multi_bit_strength.v
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+
+module top;
+  parameter length = 16;
+  reg [length*8-1:0] result;
+  wire [3:0] net;
+
+  assign (pull1, strong0) net = 4'b0110;
+
+  initial begin
+    $swrite(result, "%v", net);
+    $display("All three lines should match:");
+    $display("-----------------------------");
+    $display("St0_Pu1_Pu1_St0 (reference)");
+    $display("%v (display)\n%0s (swrite)", net, result);
+  end
+endmodule
