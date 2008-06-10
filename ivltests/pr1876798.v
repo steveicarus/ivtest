38870ec6c5fb47670a914fa4e853346745070ff9
stevewilliams <stevewilliams>
2008-06-10 04:33:41 +0000

    Use work directory for temp file.

--- ivltests/pr1876798.v
+++ ivltests/pr1876798.v
@@ -11,13 +11,13 @@ integer n;
 integer v[0:3];
 
 initial begin
-  f = $fopen("temp", "w");
+  f = $fopen("work/temp.txt", "w");
   for (i = 0; i < 4; i = i + 1) begin
     $fdisplay(f, "%d", i);
   end
   $fclose(f);
 
-  f = $fopen("temp", "r");
+  f = $fopen("work/temp.txt", "r");
   for (i = 0; i < 4; i = i + 1) begin
     n = $fscanf(f, " %d ", v[i]);
   end
