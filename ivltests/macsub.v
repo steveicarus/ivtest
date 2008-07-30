43cf7faffc040a52a9135df6f4473525a12c75c4
stevewilliams <stevewilliams>
2008-07-30 21:54:32 +0000

    Add macsub test.

---
+++ ivltests/macsub.v
@@ -0,0 +1,24 @@
+
+module test();
+
+integer j;
+integer jel;
+integer x;
+integer xel;
+
+`define A(j) (jel == 1 && j == 2)
+
+initial
+begin
+	j = 0;
+	jel = 1;
+	x = 2;
+	xel = 3;
+
+	if(`A(x))
+		$display("PASSED");
+	else
+		$display("FAILED");
+end
+endmodule
+
