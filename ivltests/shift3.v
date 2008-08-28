6a8e563fff5e25a5faed5c6766194d58b129c35b
stevewilliams <stevewilliams>
2008-08-28 05:05:54 +0000

    Fix incorrect shift3 test.

--- ivltests/shift3.v
+++ ivltests/shift3.v
@@ -11,12 +11,15 @@ module main;
       xu = xs >>> 3;
       xs = xu >>> 3;
 
-      if (xu !== 8'b0001_1000) begin
+      // IEEE1364-2005 Secion 5.5.1 Rules for Expression Types
+      // "Expression type depends only on the operands. It does not depend
+      // on the left-hand side (if any)."
+      if (xu !== 8'b1111_1000) begin
 	 $display("FAILED -- xu = xs >>> 3 failed. xu=%b", xu);
 	 $finish;
       end
 
-      if (xs !== 8'b0001_1000) begin
+      if (xs !== 8'b0001_1111) begin
 	 $display("FAILED -- xs = xu >>> 3 failed. xs=%b", xs);
 	 $finish;
       end
