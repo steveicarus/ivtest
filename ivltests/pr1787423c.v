c5d177606eae532c51166e7621b161edc186970a
stevewilliams <stevewilliams>
2008-09-17 04:03:46 +0000

    Add pr1787423c.

---
+++ ivltests/pr1787423c.v
@@ -0,0 +1,32 @@
+
+module top;
+
+   wire net1, net2;
+   reg [1:0] data;
+
+   buf bus_drv[1:0] (net1, net2, data);
+
+   initial begin
+      data = 0;
+      #1 $monitor(net1,,net2,,data);
+      #1 if (net1 !== 0) begin
+	 $display("FAILED");
+	 $finish;
+      end
+
+      data = 3;
+      #1 if (net1 !== 1) begin
+	 $display("FAILED");
+	 $finish;
+      end
+
+      data = 1;
+      #1 if (net1 !== 'bx) begin
+	 $display("FAILED");
+	 $finish;
+      end
+
+      $display("PASSED");
+   end // initial begin
+
+endmodule
