1b3ee22a180594e7d40bcd7fe49a43082e13866e
stevewilliams <stevewilliams>
2008-11-18 03:51:11 +0000

    Add pr2281519 to vhdl tests.

---
+++ vhdl_tests/pr2281519.v
@@ -0,0 +1,10 @@
+
+module test();
+   wire clk;
+   reg  [8:0] 	lowp2_tmp;  
+   reg  [8:0] 	lowp2_out;
+  always @(posedge clk) begin	 
+     lowp2_out <= ( {lowp2_tmp[8], lowp2_tmp} ) >> 1;
+  end
+endmodule
+
