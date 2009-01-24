f70c5b73bada70d3384a7e0958c936dfa1f74b78
nickga <nickga>
2009-01-24 18:50:31 +0000

    Add VHDL test for pr2531370

--- vhdl_tests/generics.v
+++ vhdl_tests/generics.v
@@ -1,10 +1,11 @@
 // A few simple tests of translating parameters to generics
 module top();
   wire [7:0] v1, v2, v3;
+  wire [7:0] w1, w2, w3;
   
-  child c1(v1);
-  child c2(v2);
-  child c3(v3);
+  child c1(v1, w1);
+  child c2(v2, w2);
+  child c3(v3, w3);
 
   defparam c1.MY_VALUE = 6;
   defparam c2.MY_VALUE = 44;
@@ -26,11 +27,13 @@ module top();
   
 endmodule // top
 
-module child(value);
-  output [7:0] value;
-  reg    [7:0] value;    
+module child(value, value_w);
+  output [7:0] value, value_w;
+  reg [7:0]    value;
   
   parameter MY_VALUE = 3;
+
+  assign value_w = MY_VALUE;
 
   // Make a non-trivial process
   initial begin
