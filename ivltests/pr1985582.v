1be4db8abe41cb0af36f046f51d7dfd55fa4fdf3
Cary R <cygcary@yahoo.com>
2009-05-05 16:19:32 -0700

    Add more checks to pr1985582 (%t and real func, etc.)

--- ivltests/pr1985582.v
+++ ivltests/pr1985582.v
@@ -2,13 +2,22 @@
 
 module top;
   realtime rtime;
-  time ctime;
+  time itime;
 
   initial begin
     repeat (5) begin
-      $display("%4t %.2f %2d %4t %.2f", $time, $time, $time,
-               $realtime, $realtime,, $time,, $realtime);
+      rtime = $realtime;
+      itime = $time;
+      $display("%4t %.2f %2d, %4t %0.2f %2d, %4t %.2f, %4t %0.2f, %4t %.2f,",
+               $time, $time, $time, itime, itime, itime,
+               $realtime, $realtime, rtime, rtime, rtm($realtime),
+               rtm($realtime),, $time,, $realtime);
       #0.6;
     end
   end
+
+  function real rtm;
+    input real rin;
+    rtm = rin;
+  endfunction
 endmodule
