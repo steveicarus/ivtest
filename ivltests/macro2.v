236d38cf150e44035addea045eb84954442d83c6
Joe Auricchio <jauricchio@gmail.com>
2010-11-27 22:52:46 -0500

    Test that `resetall does not clear macros
    Per Verilog 2001 spec 19.3

--- ivltests/macro2.v
+++ ivltests/macro2.v
@@ -1,5 +1,9 @@
 `define TBMESS(str) $display("MSG: %m - %s", str );
 
+ // 1364-2001 S19.3 "The text macro facility is not affected by the compiler
+ // directive `resetall."
+`resetall
+
 module main;
 
 initial `TBMESS("This is the message for this example.")
