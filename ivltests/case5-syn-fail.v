b813bd246573956111609e04a924b85a5bb1f7cd
Cary R <cygcary@yahoo.com>
2010-12-23 18:45:16 -0800

    Fix invalid case5 test.
    The original case5 test is invalid since the code incorrect drives
    the output from both synthesized and non-synthesized code. Fixing
    this allows V0.8 to synthesize this test correctly. V0.8 has also
    been enhanced to print an error message and fail if this happens.
    
    The vpi/memmon test was also marked NI for V0.8 since it fails
    because of missing functionality that will not be added to V0.8.

--- ivltests/case5-syn-fail.v
+++ ivltests/case5-syn-fail.v
@@ -4,12 +4,11 @@
  */
 module main;
 
-   reg clk, bit, foo, clr;
+   reg clk, bit, foo;
 
    // Synchronous device that toggles whenever enabled by a high bit.
-   always @(posedge clk or posedge clr)
-     if (clr) foo = 0;
-     else case (bit)
+   always @(posedge clk)
+     case (bit)
        1'b0: ;
        1'b1: foo <= ~foo;
      endcase // case(bit)
@@ -24,13 +23,12 @@ module main;
    initial begin
       clk = 0;
       bit = 0;
-      clr = 1;
+      foo = 0;
       # 6 $display("clk=%b, bit=%b, foo=%b", clk, bit, foo);
       if (bit !== 0 || foo !== 0) begin
 	 $display("FAILED");
 	 $finish;
       end
-      clr = 0;
 
       #10 $display("clk=%b, bit=%b, foo=%b", clk, bit, foo);
       if (bit !== 0 || foo !== 0) begin
