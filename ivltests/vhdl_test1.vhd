14bdea532f31d1a3aa97c06afc1ae477df1f1f7b
Stephen Williams <steve@icarus.com>
2011-08-20 12:11:06 -0700

    Add vhdl_test1 test.

---
+++ ivltests/vhdl_test1.vhd
@@ -0,0 +1,26 @@
+--
+-- Author: Pawel Szostek (pawel.szostek@cern.ch)
+-- Date: 28.07.2011
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity mask is
+    port (input : in std_logic_vector(15 downto 0);
+          output : out std_logic_vector(15 downto 0)
+    );
+end;
+
+architecture behaviour of mask is
+begin
+    L: process(input)
+        variable tmp : std_logic_vector(15 downto 0);
+    begin
+        output <= tmp; --this shouln't really change anything
+        tmp := input;
+        tmp := tmp and "1010101010101010";
+        output <= tmp;
+    end process;
+end;
