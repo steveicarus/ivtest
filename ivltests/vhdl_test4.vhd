6eb5950ba13b5fb806b5247014713b35a3e39624
Stephen Williams <steve@icarus.com>
2011-09-11 17:08:48 -0700

    Add vhdl_test4 test.

---
+++ ivltests/vhdl_test4.vhd
@@ -0,0 +1,23 @@
+--
+-- Author: Pawel Szostek (pawel.szostek@cern.ch)
+-- Date: 27.07.2011
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+USE ieee.numeric_std.all;
+
+entity dummy is
+    port (o1: out std_logic_vector(7 downto 0);
+          o2: out std_logic_vector(7 downto 0);
+          o3: out std_logic_vector(7 downto 0)
+    );
+end;
+
+architecture behaviour of dummy is
+begin
+    o1 <= (others => '0');
+    o2 <= (3 => '1', others => '0');
+    o3 <= (7=>'1', 6|5|4|3|2|1|0 => '0', others => '1'); --tricky
+    
+end;
