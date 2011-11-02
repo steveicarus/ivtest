acd33ad681b34dee3ae8c97a9bcd1932f37abf3c
Martin Whitaker <mailing-list@martin-whitaker.me.uk>
2011-11-02 20:39:49 +0000

    Test suite fix for MinGW.
    MinGW does not provide a bzero() function, so use memset instead.

--- vpi/final.c
+++ vpi/final.c
@@ -1,5 +1,5 @@
 #include  <vpi_user.h>
-#include  <strings.h>
+#include  <string.h>
 
 static PLI_INT32 end_of_sim_cb(struct t_cb_data *x)
 {
@@ -10,7 +10,7 @@ static PLI_INT32 end_of_sim_cb(struct t_cb_data *x)
 static void cb_register()
 {
       s_cb_data cb_data;
-      bzero(&cb_data, sizeof(s_cb_data));
+      memset(&cb_data, 0, sizeof(s_cb_data));
       cb_data.reason = cbEndOfSimulation;
       cb_data.cb_rtn = end_of_sim_cb;
       vpi_register_cb(&cb_data);
