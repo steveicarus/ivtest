30e5e0d3604c8012103af9ce60a25069ac4c80ad
Stephen Williams <steve@icarus.com>
2011-11-17 14:37:19 -0800

    Document Modelsim VHDL results differences.

--- ivltests/test_signal_init_assign.v
+++ ivltests/test_signal_init_assign.v
@@ -5,9 +5,28 @@ module main;
    reg clk;
    wire dst0, dst1;
 
-   test #(.parm(0)) test0 (.dst(dst0), .src(src), .clk(clk));
-   test #(.parm(1)) test1 (.dst(dst1), .src(src), .clk(clk));
+test #(.parm(0)) test0 (.dst(dst0), .src(src), .clk(clk));
+test #(.parm(1)) test1 (.dst(dst1), .src(src), .clk(clk));
 
+//Note: For Modelsim compatibility do instantiation as:
+//test #(.parm(2'b10)) test0 (.dst(dst0), .src(src), .clk(clk));
+//test #(.parm(2'b11)) test1 (.dst(dst1), .src(src), .clk(clk));
+//The reason is that Modelsim handles single-bit std_logic as an
+//enumeration, and enumeration values 2 and 3 correspond to the
+//stdlogic '0' and '1' values. The integer to std_logic values
+//in modelsim are:
+   //  0   - 'U'
+   //  1   - 'X'
+   //  2   - '0'
+   //  3   - '1'
+   //  4   - 'Z'
+   //  5   - 'W'
+   //  6   - 'L'
+   //  7   - 'H'
+   //  8   - '-'
+//Maybe in the future we'll have to do something similar?
+
+   
    initial begin
       clk = 0;
       src = 0;
@@ -25,5 +44,8 @@ module main;
       end
       $display("PASSED");
    end // initial begin
+   
+   
+
 
 endmodule // main
