291e14ef051c14d0ec246ceb91ef7995a08fe394
Iztok Jeras <iztok.jeras@gmail.com>
2012-03-25 20:57:00 +0200

    added another test for packed structures

---
+++ ivltests/struct_packed_sysfunct.v
@@ -0,0 +1,28 @@
+
+// This tests system functions available for packed structures
+
+module test;
+
+   typedef struct packed {
+      logic [7:0] high;
+      logic [7:0] low;
+   } word_t;
+
+   // Declare word0 as a VARIABLE
+   word_t word0;
+
+   // error counter
+   int err = 0;
+
+   initial begin
+      #1;
+      //
+      if ($bits(word0     ) !== 16) begin $display("FAILED -- $bits(word0     ) = %d", $bits(word0     )); err=err+1; end
+      if ($bits(word0.high) !==  8) begin $display("FAILED -- $bits(word0.high) = %d", $bits(word0.high)); err=err+1; end
+      if ($bits(word0.low ) !==  8) begin $display("FAILED -- $bits(word0.low ) = %d", $bits(word0.low )); err=err+1; end
+
+      if (err)  $finish();
+      else      $display("PASSED");
+   end
+
+endmodule // test
