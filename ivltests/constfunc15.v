eb9483b4a57e32082f848fe2f737a712e9ce9732
Martin Whitaker <icarus@martin-whitaker.me.uk>
2013-05-18 21:27:28 +0100

    Added tests for assignment LHS out-of-range/undefined word selects.

--- ivltests/constfunc15.v
+++ ivltests/constfunc15.v
@@ -12,12 +12,38 @@ end
 
 endfunction
 
-localparam res1 = concat1(8'h5a);
+function [7:0] concat2(input [7:0] value);
+
+reg [3:0] tmp[1:2];
+
+begin
+  {tmp[1], tmp[3]} = {value[3:0], value[7:4]};
+  {concat2[3:0], concat2[7:4]} = {tmp[3], tmp[1]};
+end
+
+endfunction
+
+function [7:0] concat3(input [7:0] value);
+
+reg [3:0] tmp[1:2];
+
+begin
+  {tmp['bx], tmp[1]} = {value[3:0], value[7:4]};
+  {concat3[3:0], concat3[7:4]} = {tmp['bx], tmp[1]};
+end
+
+endfunction
+
+localparam res1 = concat1(8'ha5);
+localparam res2 = concat2(8'ha5);
+localparam res3 = concat2(8'ha5);
 
 reg failed = 0;
 
 initial begin
-  $display("%h", res1); if (res1 !== 8'ha5) failed = 1;
+  $display("%h", res1); if (res1 !== 8'h5a) failed = 1;
+  $display("%h", res2); if (res2 !== 8'h5x) failed = 1;
+  $display("%h", res3); if (res3 !== 8'h5x) failed = 1;
 
   if (failed)
     $display("FAILED");
