d208eb776bbc0f3fae9af318706bb86393b031d2
Martin Whitaker <icarus@martin-whitaker.me.uk>
2013-05-27 20:17:04 +0100

    Improved regression test for bug 931.

--- ivltests/br931.v
+++ ivltests/br931.v
@@ -3,7 +3,7 @@ module bug();
 reg flag1;
 reg flag2;
 
-task set_flag1(inout flag);
+task task1(inout integer flag);
 
 begin
   #4 flag = 1;
@@ -11,7 +11,7 @@ end
 
 endtask
 
-task set_flag2(inout flag);
+task task2(inout integer flag);
 
 begin
   #5 flag = 1;
@@ -19,13 +19,21 @@ end
 
 endtask
 
+task task3(inout flag1, inout flag2);
+
+fork
+  task1(flag1);
+  task2(flag2);
+join
+
+endtask
+
 initial begin
   flag1 = 0;
   flag2 = 0;
-  fork
-    set_flag1(flag1);
-    set_flag2(flag2);
-  join
+  task3(flag1, flag2);
+  $display("flag1 = %d", flag1);
+  $display("flag2 = %d", flag2);
   if ((flag1 === 1) && (flag2 === 1))
     $display("PASSED");
   else
