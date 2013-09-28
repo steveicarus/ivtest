99b5ca41651ca7e6c3858641ed2cc261863d1513
Martin Whitaker <icarus@martin-whitaker.me.uk>
2013-09-28 17:37:14 +0100

    Added regression test for br937.

---
+++ ivltests/br937.v
@@ -0,0 +1,17 @@
+module top;
+
+localparam [8*8:0] expect = "0123456789";
+
+reg [8*8:0] str;
+reg passed;
+
+initial begin
+  passed = $value$plusargs("string=%s", str);
+  $display("%h", str);
+  if (passed && (str == expect))
+    $display("PASSED");
+  else
+    $display("FAILED");
+end
+
+endmodule
