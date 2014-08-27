314c13e177b510d7d57d4d244fe56dc8fab033ab
Maciej Suminski <maciej.suminski@cern.ch>
2014-08-27 12:02:04 +0200

    vhdl_init: Corrected comparison operators. Updated reports.

--- ivltests/vhdl_init.v
+++ ivltests/vhdl_init.v
@@ -24,17 +24,17 @@ module vhdl_init_testbench;
 
   initial begin
     // Simply check if the assigned values are correct
-    if (dut.a != 'b11101001) begin
+    if (dut.a !== 'b11101001) begin
       $display("FAILED #1: expected 11101001, got %b", dut.a);
       $finish;
     end
 
-    if (dut.b != 'b1010) begin
+    if (dut.b !== 'b1010) begin
       $display("FAILED #2: expected 1010, got %b", dut.b);
       $finish;
     end
 
-    if (dut.c != 'b1000) begin
+    if (dut.c !== 'b1000) begin
       $display("FAILED #3: expected 1000, got %b", dut.c);
       $finish;
     end
