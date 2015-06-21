87dd158cac70163b5789c48dfc17c93f5f7fa53e
Martin Whitaker <mailing-list@martin-whitaker.me.uk>
2015-06-21 09:29:12 +0100

    More tests for task/function arguments.

---
+++ ivltests/br982.v
@@ -0,0 +1,18 @@
+module example;
+  task simple_task;
+    input in;
+    output out;
+    begin
+      out = in;
+    end
+  endtask
+
+  reg x = 0;
+  initial begin
+    simple_task(x);
+    $finish;
+  end
+endmodule
+
+
+  
---
+++ ivltests/br982a.v
@@ -0,0 +1,17 @@
+module example;
+  function simple_func;
+    input in;
+    begin
+      simple_func = in;
+    end
+  endfunction
+
+  reg x = 0;
+  initial begin
+    x = simple_func(x,x);
+    $finish;
+  end
+endmodule
+
+
+  
