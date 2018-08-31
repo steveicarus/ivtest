d6ae8e26b9594e1ba56e9342f91e39703edee4a6
Cary R <cygcary@yahoo.com>
2018-08-30 22:24:27 -0700

    Add tests for bit vector system functions

---
+++ ivltests/sf_countbits_fail.v
@@ -0,0 +1,14 @@
+module top;
+  integer result;
+
+  initial begin
+
+    result = $countbits(top);
+    result = $countbits("a string");
+    result = $countbits(1'bx);
+    result = $countbits(1'bx, "a string");
+    result = $countbits(1'bx, 1'bx, "a string");
+
+  end
+
+endmodule
