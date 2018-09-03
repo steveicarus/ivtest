92abd68be0668ee9087227ab185cfdaf2710367a
Cary R <cygcary@yahoo.com>
2018-09-03 08:39:01 -0700

    Update for the changes in the bit vector functions

--- ivltests/sf_countbits_fail.v
+++ ivltests/sf_countbits_fail.v
@@ -6,6 +6,7 @@ module top;
     result = $countbits(top);
     result = $countbits("a string");
     result = $countbits(1'bx);
+    result = $countbits("a string", 1'bx);
     result = $countbits(1'bx, "a string");
     result = $countbits(1'bx, 1'bx, "a string");
 
