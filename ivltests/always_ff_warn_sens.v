f85673760f3b60336af84b29e60311cda58909ed
Cary R <cygcary@yahoo.com>
2018-09-23 22:41:44 -0700

    Fix space issues

--- ivltests/always_ff_warn_sens.v
+++ ivltests/always_ff_warn_sens.v
@@ -11,7 +11,7 @@ module top;
 
   // The compiler should warn that rst is missing an edge keyword.
   always_ff @(posedge clk or rst[4]) begin
-    if (rst[4]) 
+    if (rst[4])
       q2 <= 1'b0;
     else
       q2 <= d;
@@ -19,7 +19,7 @@ module top;
 
   // The compiler should warn that rst is missing an edge keyword.
   always_ff @(posedge clk or rst[i]) begin
-    if (rst[i]) 
+    if (rst[i])
       q3 <= 1'b0;
     else
       q3 <= d;
@@ -27,7 +27,7 @@ module top;
 
   // The compiler should warn that rst is missing an edge keyword.
   always_ff @(posedge clk or !rst) begin
-    if (!rst) 
+    if (!rst)
       q4 <= 1'b0;
     else
       q4 <= d;
@@ -35,7 +35,7 @@ module top;
 
   // The compiler should warn that rst is missing an edge keyword.
   always_ff @(posedge clk or ~rst[4]) begin
-    if (~rst[4]) 
+    if (~rst[4])
       q5 <= 1'b0;
     else
       q5 <= d;
@@ -43,7 +43,7 @@ module top;
 
   // The compiler should warn that rst is missing an edge keyword.
   always_ff @(posedge clk or &rst) begin
-    if (&rst) 
+    if (&rst)
       q6 <= 1'b0;
     else
       q6 <= d;
@@ -51,7 +51,7 @@ module top;
 
   // The compiler should warn that rst is not a single bit.
   always_ff @(posedge clk or posedge rst) begin
-    if (rst) 
+    if (rst)
       q7 <= 1'b0;
     else
       q7 <= d;
