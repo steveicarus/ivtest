fb2f58bcf2aac3a134b429f169b68ab078556ae7
Martin Whitaker <icarus@martin-whitaker.me.uk>
2019-09-11 23:01:53 +0100

    Add and modify tests for improved SV cast support.

--- ivltests/sv_cast_darray.v
+++ ivltests/sv_cast_darray.v
@@ -1,74 +1,68 @@
-// Copyright (c) 2014 CERN
-// Maciej Suminski <maciej.suminski@cern.ch>
-//
-// This source code is free software; you can redistribute it
-// and/or modify it in source code form under the terms of the GNU
-// General Public License as published by the Free Software
-// Foundation; either version 2 of the License, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+module test();
 
+typedef bit     [63:0] bit64;
+typedef logic   [63:0] vec64;
 
-// Test for casting a dynamic array to vector type
-// using Icarus specific VPI functions
+byte            byte_array [];
+bit     [15:0]  bit_array  [];
+logic   [31:0]  vec_array  [];
+real            real_array [];
 
-module sv_cast_string();
-    bit [7:0] darr [];
-    bit [63:0] darr_64 [];
-    bit [7*8 - 1:0] arr;
-    bit [127:0] arr_128;
+bit64           bit_result;
+vec64           vec_result;
+
+reg failed = 0;
 
 initial begin
-    darr_64 = new[2];
-    darr_64[0] = "ABCDEFGH";
-    darr_64[1] = "IJKLMNOP";
+  byte_array = new [8];
+  foreach (byte_array[i]) byte_array[i] = i*16 + i;
 
-    darr = new[7];
-    // Set darr to '{"a","b","c","d","e","f","g"}
-    foreach(darr[i])
-        darr[i] = "a" + i;
+  bit_result = bit64'(byte_array);
+  $display("%h", bit_result);
+  if (bit_result !== 64'h0011223344556677) failed = 1;
 
-    // Casting dynamic array to vector
-    $ivl_darray_method$to_vec(darr, arr);
-    if(arr !== "abcdefg") begin
-        $display("FAILED 1");
-        $finish();
-    end
+  vec_result = vec64'(byte_array);
+  $display("%h", vec_result);
+  if (vec_result !== 64'h0011223344556677) failed = 1;
 
-    $ivl_darray_method$to_vec(darr_64, arr_128);
-    if(arr_128 !== "ABCDEFGHIJKLMNOP") begin
-        $display("FAILED 2");
-        $finish();
-    end
+  bit_array = new [4];
+  foreach (bit_array[i]) bit_array[i] = i*4096 + i*256 + i*16 + i;
 
-    // Reset the stored data to perform reverse casting test
-    arr = "0123456";
-    arr_128 = "cafedeadbeefc0de";
+  bit_result = bit64'(bit_array);
+  $display("%h", bit_result);
+  if (bit_result !== 64'h0000111122223333) failed = 1;
 
-    // Casting vector to dynamic array
-    $ivl_darray_method$from_vec(darr, arr);
-    foreach(darr[i]) begin
-        if(darr[i] != "0" + i) begin
-            $display("FAILED 3");
-            $finish();
-        end
-    end
+  vec_result = vec64'(bit_array);
+  $display("%h", vec_result);
+  if (vec_result !== 64'h0000111122223333) failed = 1;
 
-    $ivl_darray_method$from_vec(darr_64, arr_128);
-    if(darr_64[0] !== "cafedead" || darr_64[1] !== "beefc0de") begin
-        $display("FAILED 4");
-        $finish();
-    end
+  vec_array = new [2];
+  vec_array[0] = 32'b01xz_0001_0010_0011_0100_0101_0110_0111;
+  vec_array[1] = 32'b1000_1001_1010_1011_1100_1101_1110_1111;
 
+  bit_result = bit64'(vec_array);
+  $display("%h", bit_result);
+  if (bit_result !== 64'b0100_0001_0010_0011_0100_0101_0110_0111_1000_1001_1010_1011_1100_1101_1110_1111) failed = 1;
+
+  vec_result = vec64'(vec_array);
+  $display("%h", vec_result);
+  if (vec_result !== 64'b01xz_0001_0010_0011_0100_0101_0110_0111_1000_1001_1010_1011_1100_1101_1110_1111) failed = 1;
+
+  real_array = new [1];
+  real_array[0] = 1.2345678;
+
+  bit_result = bit64'(real_array);
+  $display("%h", bit_result);
+  if (bit_result !== $realtobits(1.2345678)) failed = 1;
+
+  vec_result = vec64'(real_array);
+  $display("%h", vec_result);
+  if (vec_result !== $realtobits(1.2345678)) failed = 1;
+
+  if (failed)
+    $display("FAILED");
+  else
     $display("PASSED");
 end
-endmodule
 
+endmodule
--- ivltests/sv_cast_string.v
+++ ivltests/sv_cast_string.v
@@ -28,16 +28,12 @@ module sv_cast_string();
     int i;
     str = "0123456";
     chars = strbits'(str);
-
-    // This is Icarus-specific system function that in does the casting
-    $ivl_string_method$to_vec(str, chars);
     if(chars != 56'h30313233343536)
     begin
         $display("FAILED 1 chars = %x", chars);
         $finish();
     end
 
-    // Casting in the way defined in the standard
     str = "6543210";
     chars = strbits'(str);
     if(chars != "6543210")
@@ -58,4 +54,3 @@ module sv_cast_string();
     $display("PASSED");
 end
 endmodule
-
