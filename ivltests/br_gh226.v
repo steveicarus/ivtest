3e8726a2438fd11e8e5e49ff0dae3634dcf6fee2
Stephen Williams <steve@icarus.com>
2019-09-27 13:51:17 -0700

    br_gh226 should now pass.

--- ivltests/br_gh226.v
+++ ivltests/br_gh226.v
@@ -6,6 +6,12 @@ struct packed {
 
 initial begin
   data.value[7:0] = 8'h55;
+  data.value[15:8] = 8'haa;
+  if (data !== 16'haa55) begin
+    $display("FAILED -- data=%h", data);
+    $finish;
+  end
+  $display("PASSED");
 end
 
 endmodule
