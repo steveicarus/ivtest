24e9bbabbb83834ee813d5c3272ba0755a0853a5
Stephen Williams <steve@icarus.com>
2019-10-18 10:55:03 -0700

    Fix tests of the $fatal system task.

--- ivltests/fatal_et_al2.v
+++ ivltests/fatal_et_al2.v
@@ -3,6 +3,7 @@ module top;
     #1 $info("This is the $info message.");
     #1 $warning("This is the $warning message.");
     #1 $error("This is the $error message.");
-    #1 $display("Check that the messages are correct.");
+    #1 $fatal(0, "This is the $fatal message.");
+    #1 $display("FAILED: This should not print.");
   end
 endmodule
