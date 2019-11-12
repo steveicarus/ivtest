f642e2493e969b0d0779d41400fcab5155a6ce19
Martin Whitaker <icarus@martin-whitaker.me.uk>
2019-11-12 18:15:14 +0000

    The spelling of pulsere_flag is fixed in V11.

--- vpi/spec_delays.c
+++ vpi/spec_delays.c
@@ -80,7 +80,11 @@ static PLI_INT32 dump_specify_calltf(PLI_BYTE8*name)
 		  delays.time_type = vpiSimTime;
 		  delays.mtm_flag = 0;
 		  delays.append_flag = 0;
+#ifdef IVERILOG_V10
 		  delays.plusere_flag = 0;
+#else
+		  delays.pulsere_flag = 0;
+#endif
 		  vpi_get_delays(item, &delays);
 		  vpi_printf("**        (%d,%d,%d, %d,%d,%d, %d,%d,%d, %d,%d,%d)\n",
 			     (int)delay_times[0].low,
@@ -101,7 +105,11 @@ static PLI_INT32 dump_specify_calltf(PLI_BYTE8*name)
 		  delays.time_type = vpiScaledRealTime;
 		  delays.mtm_flag = 0;
 		  delays.append_flag = 0;
+#ifdef IVERILOG_V10
 		  delays.plusere_flag = 0;
+#else
+		  delays.pulsere_flag = 0;
+#endif
 		  vpi_get_delays(item, &delays);
 		  vpi_printf("**        (%f,%f,%f, %f,%f,%f, %f,%f,%f, %f,%f,%f)\n",
 			     delay_times[0].real,
@@ -137,7 +145,11 @@ static PLI_INT32 dump_specify_calltf(PLI_BYTE8*name)
 		  delays.time_type = vpiScaledRealTime;
 		  delays.mtm_flag = 0;
 		  delays.append_flag = 0;
+#ifdef IVERILOG_V10
 		  delays.plusere_flag = 0;
+#else
+		  delays.pulsere_flag = 0;
+#endif
 		  vpi_get_delays(item, &delays);
 		  vpi_printf("**        (%f,%f,%f, %f,%f,%f, %f,%f,%f, %f,%f,%f)\n",
 			     delay_times[0].real,
