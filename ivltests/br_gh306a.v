3fd4015ccbe198024b7d2db2677bb60a46da3577
Martin Whitaker <icarus@martin-whitaker.me.uk>
2020-02-09 18:41:36 +0000

    Add regression tests for br_gh306.

---
+++ ivltests/br_gh306a.v
@@ -0,0 +1,20 @@
+module counter(out, clk, reset);
+
+  parameter WIDTH = 8;
+
+  output [WIDTH-1 : 0] out;
+  input            clk, reset;
+
+  reg [WIDTH-1 : 0]   out;
+  wire         clk, reset;
+
+  always @(posedge clk)
+    out <= out + 1;
+
+  always @(posedge reset)
+    assign out = 0;
+
+  always @(negedge reset)
+    deassign out;
+
+endmodule // counter
