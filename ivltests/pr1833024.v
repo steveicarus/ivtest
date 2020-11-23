39b02d12fe929876a116f56dea5edcccbd47e493
Cary R <cygcary@yahoo.com>
2020-11-22 22:33:15 -0800

    Add some more `begin_keywords

--- ivltests/pr1833024.v
+++ ivltests/pr1833024.v
@@ -1,3 +1,4 @@
+`begin_keywords "1364-2005"
 module top;
   reg svar;
   reg sarr [1:0];
@@ -89,3 +90,4 @@ module submod3(arg1, arg2, arg3, arg4);
 
   initial $display("In submod3 with %b, %b, %b, %b", arg1, arg2, arg3, arg4);
 endmodule
+`end_keywords
